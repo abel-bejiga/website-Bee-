5d22f7d2c3bf25a35a6bff9083b68b0660bbcde0
Abel Bejiga <104611692+abel-bejiga@users.noreply.github.com>
2023-03-22 01:13:28 -0400

    fixing code

---
+++ index.js
@@ -0,0 +1,25 @@
+
+
+
+$("#gallary").click(function(){
+    
+    $("#bg-cover-add").removeClass("hidden")
+
+        $("#hero-disc").addClass("out");
+        $("#hero-disc").removeClass("in");
+
+
+        // each pictures on click
+        
+ 
+})
+
+
+// $(".hero").click(function(){
+    
+//     $("#bg-cover-add").addClass("hidden").addClass("in")
+//     $("#hero-disc").removeClass("out");
+
+   
+// })
+console.log(4+3) 
